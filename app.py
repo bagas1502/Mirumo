c4b61967070ecad77d13d83210de46fdbd4382e3
bagas1502 <bagas15022010@mail.ru>
2025-07-06 16:19:26 +0500

    Добавлен шаблон и CSS

--- app.py
+++ app.py
@@ -129,7 +129,3 @@ if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-
-@app.route('style.css')
-def css():
-    return open("style.css", "r").read(), 200, {'Content-Type': 'text/css'}
